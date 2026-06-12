3fd3c6a82e453ea4a84f4729435ee4434b558d30
Gekkumo <Gekkumo@gmail.com>
2026-06-12 21:51:40 +0300

    feat: add EventProcessingException for technical errors with cause preservation

--- src/main/java/beacon/common/exception/EventProcessingException.java
+++ src/main/java/beacon/common/exception/EventProcessingException.java
@@ -2,10 +2,6 @@ package beacon.common.exception;
 
 public class EventProcessingException extends RuntimeException {
 
-    public EventProcessingException(String message) {
-        super(message);
-    }
-
     public EventProcessingException(String message, Throwable cause) {
         super(message, cause);
     }
